ecbd974d0b3305d0831f9d39bd23c272d19e8d5d
Alexander <nemechekalexander@gmail.com>
2019-02-06 11:43:43 +0200

    new

--- src/Projects.js
+++ src/Projects.js
@@ -63,7 +63,7 @@ const Projects = () => {
                 <a href = "https://newschannel.netlify.com/" >
                 <img className ="shell" alt = '' src = {news}/>
                 </a>
-                <p className = 'exchangeText'>A compact, single page REACT application displaying articles through Axios requests made to over 30 news APIs updated live.</p>
+                <p className = 'exchangeText'>Daily News, a compact, single page REACT application displaying articles through Axios requests made to over 30 news APIs, updated live.</p>
                 <a className = 'git1' href = "https://github.com/alexnmc/daily-news ">
                     <div className = "git2"></div>
                 </a>
