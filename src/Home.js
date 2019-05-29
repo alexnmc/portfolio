ee344e154ae9fe9bf621afe9ae6b841b8c02e628
Alexander <nemechekalexander@gmail.com>
2019-05-29 17:08:31 +0300

    email form added

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ const Home = props => {
         <div className="home"  id = '1'>
             <div className = "home1"> 
                 <div className = "home2">
-                    <h1 className = 'navbarP2'> Alexander Nemechek </h1>
+                    <h1 className = 'navbarP2'>Alexander Nemechek </h1>
                     <p  className = 'welcomeP'>Welcome to my portfolio website</p>
                     <h5>this site was made with REACT</h5>
                     <div className = "react1"></div>
