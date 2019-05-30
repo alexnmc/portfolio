ffe30c3a99c7295d1fe0fe14c91f7c5b83fd42be
Alexander <nemechekalexander@gmail.com>
2019-05-30 19:35:55 +0300

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import './style.css'
-require('dotenv').config()
 
 ReactDOM.render(
     <BrowserRouter>
