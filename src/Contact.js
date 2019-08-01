e2e3b9678fa8963ff91a6338411091de16b5c657
Alexander <nemechekalexander@gmail.com>
2019-08-02 02:22:37 +0300

    update

--- src/Contact.js
+++ src/Contact.js
@@ -6,7 +6,8 @@ const Contact = () => {
     return (
         <div className = "contact" id = '4' >
             <div className = "contact1">
-                <h1>Contact me: </h1>
+                <h1 className = 'conAir'>Contact me:</h1>
+                <h1 className = 'h1'>Phone: 506-501-4008</h1>
                 <ReactContactForm  className = 'contactForm' to="nemechekalexander@gmail.com" />
             </div>
         </div>
