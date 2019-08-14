73fb6a8d6ca8d90d0fda1ffd765493319b910613
Alexander <nemechekalexander@gmail.com>
2019-08-14 07:24:38 +0300

    new

--- src/About.js
+++ src/About.js
@@ -1,15 +1,33 @@
 import React from 'react'
-
+import resume from './Photos/resume.pdf'
 
 
 const About = () => {
     
     return (
         <div className="about" id = '2'>
+            
             <div className = "home3">
-                
-                <div className = "ID">
-                </div>
+                <div className = "ID"></div>
+                <div className = 'inline'>
+                    <div className = "fixed">
+                        <a className = 'git1' href = "https://github.com/alexnmc ">
+                            <div className = "git"></div>
+                        </a>
+                    </div>
+
+                    <div className = "fixed">
+                        <a className = 'linkedin1' href = "https://www.linkedin.com/in/alexander-nemechek-10240a167">
+                            <div className = "linkedin"></div>
+                        </a>
+                    </div>
+                        
+                    <div className = "fixed">
+                        <a className = 'resume1' href = {resume} >
+                            <div className = "resume"></div>
+                        </a>
+                    </div>
+                    </div>
                     <p className = "aboutP">
                         My name is Alexander Nemechek and I am a full stack software developer.
                         I enjoy being challenged and engaging in projects that require me to work outside my comfort and knowledge set, 
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import resume from './Photos/resume.pdf'
+
 
 
 
@@ -10,27 +10,6 @@ const Home = props => {
             <div className = "home1"> 
                 <div className = "home2">
                     <h1 className = 'navbarP2'>Alexander Nemechek - <span className = 'fullstack'>full stack web developer</span> </h1>
-                    <div className = 'inline'>
-                        <div className = "fixed">
-                        <a className = 'git1' href = "https://github.com/alexnmc ">
-                            <div className = "git"></div>
-                        </a>
-                        </div>
-
-                        <div className = "fixed">
-                        <a className = 'linkedin1' href = "https://www.linkedin.com/in/alexander-nemechek-10240a167">
-                            <div className = "linkedin"></div>
-                        </a>
-                        </div>
-                        
-                        <div className = "fixed">
-                        <a className = 'resume1' href = {resume} >
-                            <div className = "resume"></div>
-                        </a>
-                        </div>
-                    </div>
-                    
-                    
                     <p  className = 'welcomeP'>Welcome to my portfolio website</p>
                     <h5>this site was made with REACT</h5>
                     <div className = "react1"></div>
