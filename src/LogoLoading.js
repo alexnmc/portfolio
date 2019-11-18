ac439704cc7ae6be21c3762a11f44685b01d822d
alex <nemechekalexander@gmail.com>
2019-11-17 23:30:25 -0600

    update

--- src/LogoLoading.js
+++ src/LogoLoading.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
 
-const LogoLoading = ({type, color}) => {
+const LogoLoading = () => {
     
   return(
-    <div style = {{height: "100vh", width: "100vw", display: "flex", alignItems: 'center', justifyContent: "center"}}>
+    <div style = {{position: "fixed", height: "100vh", width: "100vw", display: "flex", alignItems: 'center', justifyContent: "center"}}>
       <div className = "LogoLoading"></div>
     </div>
   )
 }
-
-
 
 
 export default LogoLoading
