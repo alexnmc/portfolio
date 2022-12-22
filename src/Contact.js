c1397b931d98e76f71da1a121a17e414d9bb7c1c
alex <nemechekalexander@gmail.com>
2022-12-22 13:02:15 -0500

    update

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
         <div className = "contact" id = '4' >
             <div className = "contact1">
                 <h1 className = 'conAir'>Contact me:</h1>
-                <h1 className = "h1">Tel: <a  href = {`tel:5065014008`}>506-501-4008</a></h1> 
+                <h1 className = "h1">Tel: <a  href = {`tel:9542452335`}>954-245-2335</a></h1> 
                 <ReactContactForm  className = 'contactForm' to="nemechekalexander@gmail.com" />
             </div>
         </div>
